49d42bd4a270dc97df3dc9e8f149d777cba47e0f
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-11-05 19:05:15 -0500

    maybe got the simulations figured out

--- sim_risk/hw1/dave.R
+++ sim_risk/hw1/dave.R
@@ -64,32 +64,38 @@ combined_ret <- with(sub_dat, c(crud_oil_ret, nat_gas_ret, dry_ret))
 
 combined <- data.frame(cost = combined_cost, ret = as.numeric(combined_ret)) 
 
-# Introduction to Simulation #
-set.seed(112358)
+# building the simulations
 samp <- 10000
 sim <- data.frame(y2006 = rnorm(n=samp, mean=mean(combined$ret), sd=sd(combined$ret)))
+avg_yearly_cost <- mean(combined$cost)
+sd_yearly_cost <- sd(combined$cost)
+
 
 new_years <- c('y2007', 'y2008', 'y2009', 'y2010', 'y2011', 'y2012')
-for (year in 2:7){
+for (year in 2:7) {
   avg <- mean( sim[,year - 1])
   std <- sd(sim[,year - 1])
   sim[,year] <- rnorm(n = samp, mean = avg, sd = std)
+  prev_avg <- avg_yearly_cost[year - 1]
+  avg_yearly_cost <- c(avg_yearly_cost, prev_avg * (1 + mean(sim[, year])))
 }
 colnames(sim)[2:7] <- new_years
 
 new_years2 <- c('y2013', 'y2014', 'y2015')
 for (year in 8:10){
-  sim[,year] <- rtriangle(samp, a = 0.07, b = -0.22, c = -0.0917)
+  sim[,year] <- rtriangle(samp, a = 0.07, b = 0.22, c = 0.0917)
+  prev_avg <- avg_yearly_cost[year - 1]
+  avg_yearly_cost <- c(avg_yearly_cost, prev_avg * (1 - mean(sim[, year])))
 }
 colnames(sim)[8:10] <- new_years2
 
 new_years3 <- c('y2016', 'y2017', 'y2018')
 for (year in 11:13){
-  avg <- mean(sim[, year - 1]) * 1.05
-  sim[,year] <- rtriangle(samp, a = 0.02, b = 0.06, c = avg)
+  sim[,year] <- rtriangle(samp, a = 0.02, b = 0.06, c = .05)
+  prev_avg <- avg_yearly_cost[year - 1]
+  avg_yearly_cost <- c(avg_yearly_cost, prev_avg * (1 + mean(sim[, year])))
 }
 colnames(sim)[11:13] <- new_years3
 
 
-avg <- mean(sim[, 1]) * (1-0.0917)
-sim_test <- rtriangle(samp, a = 0.02, b = 0.06, c = avg)
+avg_yearly_cost
