efe0caa99b085daf1f34b96bcbe4b16655d37f74
Chenhui Zhang <chenhuizhang@ChenhuideMacBook-Pro.local>
2018-11-18 00:26:04 -0500

    Update sim_risk/hw3/sim_hw3.R

--- sim_risk/hw3/sim_hw3.R
+++ sim_risk/hw3/sim_hw3.R
@@ -13,6 +13,7 @@
   
 #install.packages("truncnorm")
 library(truncnorm)
+library(ggplot2)
 
 ### ------------------- Simulation --------------------- ###
 hydro = vector() # a vector containing all the simulated probabilities of having hydrocarbon in a well 
@@ -26,20 +27,22 @@ for (ii in 1:10000){
   for (jj in 1:num_wells){
     # draw from random truncated normal distribution for both hydrocarbon and reservior risks
     hydrocarbon = rtruncnorm(1, a=0, b=1, mean = 0.99, sd = 0.05)
-    hydro = cbind(hydro,hydrocarbon)
+    hydro = c(hydro,hydrocarbon)
     reservior = rtruncnorm(1,a=0, b=1, mean = 0.8, sd = 0.1)
-    resv = cbind(resv,reservior)
+    resv = c(resv,reservior)
     seal = 1
     struc = 1
     # calculate the probability of being a producing well
     prob_PW = hydrocarbon*reservior*seal*struc
     # use bernoulli distribution (size =1 and n=1) to decide if it's a dry or producing well
     outcome = rbinom(1, 1, prob_PW)
-    outcome_count = cbind(outcome_count,outcome)
+    outcome_count = c(outcome_count,outcome)
   }
-  prop = cbind(prop, sum(outcome_count)/num_wells)
+  prop = c(prop, sum(outcome_count)/num_wells)
 }
 
+length(hydro)
+length(resv)
 # check out the distribution
 hist(hydro)
 hist(resv)
@@ -54,6 +57,48 @@ ES = mean(tail_values)
 
 qqnorm(prop);qqline(prop, col = 2)
 # shapiro.test(prop)
+
+### -------------- visualizations ------------------ ###
+
+prop_plot = ggplot(tibble(pred = prop), aes(pred)) +
+  geom_histogram(fill = 'lightblue', color = 'blue', bins = 60) + 
+  theme_bw() + 
+  labs(x = "Proportion of Wet Wells", 
+       y = "Frequency", 
+       title = "Figure 1: Histogram of Wet Well Proportions") +
+  theme(plot.title = element_text(hjust = 0.5,face = 'bold')) + #size = 14
+  scale_y_continuous(breaks = c(0, 100, 200, 300,400,500)) #
+
+prop_plot +
+  geom_vline(xintercept = c(VaR,ES),color="red",linetype='dashed') + 
+  geom_text(aes(x=VaR-0.08, label="5% VaR: 0.583", y=300), colour="red", angle=0) +
+  geom_text(aes(x=ES-0.08, label="5% CVaR: 0.531", y=200), colour="red", angle=0) 
+
+avg_hydro = mean(hydro)
+hydro_plot = ggplot(tibble(pred = hydro), aes(pred)) +
+  geom_histogram(fill = 'lightblue', color = 'blue', bins = 60) + 
+  ggplot2::annotate("text", x = avg_hydro-0.03, 
+           y = 9000, label = "Mean = 0.956", color = 'red', fontface = 2) + 
+  ggplot2::annotate("segment", x = avg_hydro, xend = avg_hydro, y = 0, yend = 8750, colour = "red", size = 1) +
+  theme_bw() + 
+  labs(x = "Probability of Hydrocarbons", 
+       y = "Frequency", 
+       title = "Figure 2: Histogram of Hydrocarbon Probabilities") +
+  theme(plot.title = element_text(hjust = 0.5,face = 'bold'))  
+  
+
+avg_resv = mean(resv)
+resv_plot = ggplot(tibble(pred = resv), aes(pred)) +
+  geom_histogram(fill = 'lightblue', color = 'blue', bins = 60) + 
+  ggplot2::annotate("text", x = avg_resv, 
+                    y = 9000, label = "Mean = 0.795", color = 'red', fontface = 2) + 
+  ggplot2::annotate("segment", x = avg_resv, xend = avg_resv, y = 0, yend = 8500, colour = "red", size = 1) +
+  theme_bw() + 
+  labs(x = "Probability of Reserviors", 
+       y = "Frequency", 
+       title = "Figure 3: Histogram of Reservior Probabilities") +
+  theme(plot.title = element_text(hjust = 0.5,face = 'bold')) #, size = 12
+  
 
 # --------------- some exploration...-----------------
 # truncated normal shape
